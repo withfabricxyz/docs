34ddd1e0f2ba2b35e703e0cbf4b3d9fea5764e13
Dan Simpson <dan.simpson@gmail.com>
2023-03-09 15:41:50 -0800

    Configure theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,10 +16,24 @@ const config: DocsThemeConfig = {
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/withfabricxyz/docs',
+  docsRepositoryBase: 'https://github.com/withfabricxyz/docs/tree/main/pages',
   footer: {
     text: 'Thanks for all the fish!',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Fabric Docs'
+    }
+  },
+  head: (
+    <>
+      <link rel="apple-touch-icon" href="/spark.svg" />
+      <link rel="icon" type="image/svg" href="/spark.svg"></link>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Fabric Dev Docs" />
+      <meta property="og:description" content="Documentation for the Fabric Protocol and other APIs" />
+    </>
+  ),
 }
 
 export default config
