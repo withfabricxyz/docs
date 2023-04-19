ec43cf120b2bfee745fe5fc58f7b50924ef90d3b
Dan Simpson <dan.simpson@gmail.com>
2023-04-19 08:45:35 -0700

    Update sepolia addresses

--- components/Address.tsx
+++ components/Address.tsx
@@ -5,8 +5,8 @@ const networks = {
     logic: '0x58E4036Fb5D169569627ef2F355de9d81e097E0b',
   },
   sepolia: {
-    factory: '0x04Ec36A3D4b162C4F4FB8471e3978fABBf882f40',
-    logic: '0xBE4C766225a27F2643983ba58bAFc7996Bca5e51',
+    factory: '0x83a322729C3172B0cc6Bf3a3204Fa83E683c584E',
+    logic: '0x7A5433eD0f561D7c98Fe7133F584923d9552B0E1',
   },
 };
 
