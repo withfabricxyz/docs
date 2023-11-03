4bbbb309c7de28150e36f8320af1b954dc1ffe1b
Dan Simpson <dan.simpson@gmail.com>
2023-11-03 08:35:00 -0700

    Update import statements to reflect public npm registry namespace. Add helpers for chain support.

--- components/factories.tsx
+++ components/factories.tsx
@@ -4,7 +4,12 @@ import { factories as _cfpFactories } from '@withfabric/protocol-sdks/cfpv1';
 import * as chains from 'viem/chains'
 import { Chain } from 'viem';
 
-function getChain(chainId: number) : Chain | undefined {
+type chainish = string | number;
+
+function getChain(chainId: chainish) : Chain | undefined {
+  if(typeof chainId === 'string') {
+    return Object.values(chains).find(x => x.name === chainId);
+  }
   return Object.values(chains).find(x => x.id === chainId);
 }
 
@@ -22,6 +27,9 @@ function factoryData(factories: any) {
     }
   });
 }
+
+
+
 export function cfpFactories() {
   return factoryData(_cfpFactories);
 };
@@ -29,3 +37,13 @@ export function cfpFactories() {
 export function stpFactories() {
   return factoryData(_stpFactories);
 };
+
+export function ChainList({ chains } : { chains: chainish[] }) {
+  const basic = chains.map(x => getChain(x)?.name || x).join(', ');
+
+  return <span>{basic}</span>
+}
+
+export function StpChainList() {
+  return <ChainList chains={stpFactories().map(x => Number(x.chainId))} />
+}
