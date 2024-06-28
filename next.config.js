2798203ff85296659ae5e56db04692316e18ef8a
Doug Alcantara <dougalcantara619@gmail.com>
2024-06-28 13:05:06 -0700

    add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/hypersub_coming_soon',
+        destination: '/hypersub/overview',
+        permanent: true,
+      },
+    ]
+  },
+});
